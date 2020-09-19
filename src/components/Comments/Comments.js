e5d882da62ae2117f25a2ca2952add4e8c56fb32
KVlearn <kavya.mdu@gmail.com>
2020-09-18 20:54:08 -0700

    complete

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,13 +5,16 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  console.log('comments=', props)
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+
       {comments.map((item)=>{
-        return <Comment comment={item}  />
-      })}
+        return <Comment comment={item}/>
+      })
+      }
     
     </div>
   );
